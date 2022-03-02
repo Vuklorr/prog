97e865732caad2487becc0d09781868a478136a9
Vladimir <vova.krypenichka@yandex.ru>
2022-03-02 22:54:05 +0300

    Add constructor with str

--- works/big-integer/src/big_integer.cpp
+++ works/big-integer/src/big_integer.cpp
@@ -1,8 +1,15 @@
 #include "big_integer.hpp"
 
 BigInteger::BigInteger(int& val) {
-    
+
 }
+
+BigInteger::BigInteger(std::string &str) {
+    for(int i = 0; i < str.size(); ++i) {
+        num.emplace_back(str[i] - '0');
+    }
+}
+
 
 
 
