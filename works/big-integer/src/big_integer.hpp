e211ed17c98b717200aca5ac15f70e6f1406d470
Vladimir <vova.krypenichka@yandex.ru>
2022-03-03 22:30:59 +0300

    Fixed const and add friend

--- works/big-integer/src/big_integer.hpp
+++ works/big-integer/src/big_integer.hpp
@@ -10,50 +10,64 @@ public:
     BigInteger(const int& val);
     BigInteger(const std::string& str);
 
-    const BigInteger operator+(const BigInteger& val);
-    const BigInteger operator-(const BigInteger& val);
-    const BigInteger operator*(const BigInteger& val);
-    const BigInteger operator/(const BigInteger& val);
-    const BigInteger operator%(const BigInteger& val);
+    BigInteger operator+(const BigInteger& val) const;
+    BigInteger operator-(const BigInteger& val) const;
+    BigInteger operator*(const BigInteger& val) const;
+    BigInteger operator/(const BigInteger& val) const;
+    BigInteger operator%(const BigInteger& val) const;
 
-    const bool operator <(const BigInteger& val);
-    const bool operator >(const BigInteger& val);
-    const bool operator ==(const BigInteger& val);
+    bool operator <(const BigInteger& val) const;
+    bool operator >(const BigInteger& val) const;
+    bool operator ==(const BigInteger& val) const;
 
-    const BigInteger operator+();
-    BigInteger operator-();
+    BigInteger operator+() const;
+    BigInteger operator-() const;
 
-    const BigInteger operator+=(const BigInteger& val);
-    const BigInteger operator-=(const BigInteger& val);
-    const BigInteger operator*=(const BigInteger& val);
-    const BigInteger operator/=(const BigInteger& val);
-    const BigInteger operator%=(const BigInteger& val);
+    BigInteger operator+=(const BigInteger& val) const;
+    BigInteger operator-=(const BigInteger& val) const;
+    BigInteger operator*=(const BigInteger& val) const;
+    BigInteger operator/=(const BigInteger& val) const;
+    BigInteger operator%=(const BigInteger& val) const;
 
-    const bool operator <=(const BigInteger& val);
-    const bool operator >=(const BigInteger& val);
+    bool operator <=(const BigInteger& val) const;
+    bool operator >=(const BigInteger& val) const;
 
-    const int operator+(const int& val);
-    const int operator-(const int& val);
-    const int operator*(const int& val);
-    const int operator/(const int& val);
-    const int operator%(const int& val);
+    int operator+(const int& val) const;
+    int operator-(const int& val) const;
+    int operator*(const int& val) const;
+    int operator/(const int& val) const;
+    int operator%(const int& val) const;
 
-    const int operator+=(const int& val);
-    const int operator-=(const int& val);
-    const int operator*=(const int& val);
-    const int operator/=(const int& val);
-    const int operator%=(const int& val);
+    int operator+=(const int& val) const;
+    int operator-=(const int& val) const;
+    int operator*=(const int& val) const;
+    int operator/=(const int& val) const;
+    int operator%=(const int& val) const;
 
-    const bool operator <(const int& val);
-    const bool operator >(const int& val);
-    const bool operator ==(const int& val);
+    bool operator <(const int& val) const;
+    bool operator >(const int& val) const;
+    bool operator ==(const int& val) const;
 
-    const bool operator <=(const int& val);
-    const bool operator >=(const int& val);
+    bool operator <=(const int& val) const;
+    bool operator >=(const int& val) const;
 
     std::string ToString();
 
-    friend std::ostream operator<<(std::ostream& out, const BigInteger& val);
-    friend std::istream& operator>>(std::istream& in, const BigInteger& val);
+    friend std::ostream& operator<<(std::ostream& out, const BigInteger& val);
+    friend std::istream& operator>>(std::istream& in, BigInteger& val);
     //Rule of Five
 };
+
+std::ostream& operator<<(std::ostream& out, const BigInteger& val) {
+    /*for(int i = val.num.size() - 1; i >= 0; --i) {
+        out << val.num[i];
+    }
+    return out;*/ //не работает
+}
+
+std::istream& operator>>(std::istream& in, BigInteger& val) {
+    /*for (auto i : val.num) {
+        in >> val;
+    }
+    return in;*/ //тоже не работает, но страшнее
+}
