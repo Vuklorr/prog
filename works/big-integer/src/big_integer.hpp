1b6de3d8627cd8197ffb5438db26fb5491d688ec
Vladimir <vova.krypenichka@yandex.ru>
2022-03-04 17:12:01 +0300

    Add operator '>>',but scary

--- works/big-integer/src/big_integer.hpp
+++ works/big-integer/src/big_integer.hpp
@@ -3,10 +3,13 @@
 #include <vector>
 #include <ostream>
 #include <iostream>
+#include <string>
+#include <istream>
 
 class BigInteger {
 private:
     std::vector<int> num;
+    std::string str;//для >>
 public:
     BigInteger(const int& val);
     BigInteger(const std::string& str);
@@ -54,6 +57,12 @@ public:
 
     std::string ToString();
 
+    BigInteger toInt(BigInteger& val) {//добавил для >>
+        for(int i = val.str.size() - 1; i >= 0; i--) {
+            val.num.emplace_back(val.str[i] - '0');
+        }
+        return val;
+    }
     friend std::ostream& operator<<(std::ostream& out, const BigInteger& val);
     friend std::istream& operator>>(std::istream& in, BigInteger& val);
     //Rule of Five
@@ -66,9 +75,10 @@ std::ostream& operator<<(std::ostream& out, const BigInteger& val) {
     return out;
 }
 
-std::istream& operator>>(std::istream& in, BigInteger& val) {
-    /*for (auto i : val.num) {
-        in >> val;
-    }
-    return in;*/ //не работает, но компилируется
+std::istream& operator>>(std::istream& in, BigInteger& val) {//криво - косо, с костылями, но работает
+    in >> val.str;
+
+    val.toInt(val);
+
+    return in;
 }
